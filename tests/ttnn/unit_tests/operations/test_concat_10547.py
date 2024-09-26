c8290afac944b07824ccdc5584f383430e61d12f
Jerry Wu <jerry@skymizer.com>
2024-09-26 05:28:24 +0000

    #99999: Copy concat to experimental

---
+++ tests/ttnn/unit_tests/operations/test_concat_10547.py
@@ -0,0 +1,44 @@
+import torch
+import ttnn
+
+
+def main(device):
+    input_data = torch.randn([1, 1, 32, 32], dtype=torch.bfloat16)
+    x = ttnn.from_torch(
+        input_data,
+        device=device,
+        layout=ttnn.ROW_MAJOR_LAYOUT,
+        memory_config=ttnn.L1_MEMORY_CONFIG,
+        dtype=ttnn.bfloat16,
+    )
+    input_shard_config = ttnn.create_sharded_memory_config(
+        shape=x.shape,
+        core_grid=ttnn.CoreGrid(y=2, x=1),
+        strategy=ttnn.ShardStrategy.WIDTH,
+        orientation=ttnn.ShardOrientation.ROW_MAJOR,
+        use_height_and_width_as_shard_shape=False,
+    )
+    x_t = ttnn.to_memory_config(x, memory_config=input_shard_config, dtype=ttnn.bfloat16)
+
+    output_shard_config = ttnn.create_sharded_memory_config(
+        shape=[1, 1, 32, 64],
+        core_grid=ttnn.CoreGrid(y=2, x=1),
+        strategy=ttnn.ShardStrategy.WIDTH,
+        orientation=ttnn.ShardOrientation.ROW_MAJOR,
+        use_height_and_width_as_shard_shape=False,
+    )
+    y_t = ttnn.experimental.concat([x_t, x_t], dim=3, memory_config=output_shard_config)
+    output_data = ttnn.to_torch(y_t)
+
+    print(input_data, input_data.shape)
+    print(output_data, output_data.shape)
+    print(torch.allclose(torch.concat([input_data, input_data], dim=3), output_data))
+
+
+if __name__ == "__main__":
+    device_id = 1
+    device = ttnn.open_device(device_id=device_id)
+    try:
+        main(device)
+    finally:
+        ttnn.close_device(device)
---
+++ ttnn/cpp/ttnn/operations/experimental/data_movement/concat/concat_pybind.hpp
@@ -0,0 +1,64 @@
+// SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+#include "concat.hpp"
+#include "ttnn/cpp/pybind11/decorators.hpp"
+
+namespace ttnn::operations::experimental::data_movement::detail {
+namespace py = pybind11;
+
+void bind_concat(py::module& module) {
+    const auto doc = R"doc(
+
+Args:
+    input_tensor (ttnn.Tensor): the input tensor.
+    dim (number): the concatenating dimension.
+
+Keyword Args:
+    memory_config (ttnn.MemoryConfig, optional): Memory configuration for the operation. Defaults to `None`.
+    queue_id (int, optional): command queue id. Defaults to `0`.
+    output_tensor (ttnn.Tensor, optional): Preallocated output tensor. Defaults to `None`.
+
+Returns:
+    ttnn.Tensor: the output tensor.
+
+Example:
+
+    >>> tensor = ttnn.concat(ttnn.from_torch(torch.zeros((1, 1, 64, 32), ttnn.from_torch(torch.zeros((1, 1, 64, 32), dim=3)), device)
+
+    >>> tensor1 = ttnn.from_torch(torch.zeros((1, 1, 64, 32), dtype=torch.bfloat16), device=device)
+    >>> tensor2 = ttnn.from_torch(torch.zeros((1, 1, 64, 32), dtype=torch.bfloat16), device=device)
+    >>> output = ttnn.experimental.concat([tensor1, tensor2], dim=4)
+    >>> print(output.shape)
+    [1, 1, 32, 64]
+
+    )doc";
+
+    using OperationType = decltype(ttnn::experimental::concat);
+    ttnn::bind_registered_operation(
+        module,
+        ttnn::experimental::concat,
+        doc,
+        ttnn::pybind_overload_t{
+            [](const OperationType& self,
+               const std::vector<ttnn::Tensor>& tensors,
+               const int dim,
+               std::optional<ttnn::Tensor>& optional_output_tensor,
+               std::optional<ttnn::MemoryConfig>& memory_config,
+               uint8_t queue_id) { return self(queue_id, tensors, dim, memory_config, optional_output_tensor); },
+            py::arg("tensors"),
+            py::arg("dim") = 0,
+            py::kw_only(),
+            py::arg("output_tensor").noconvert() = std::nullopt,
+            py::arg("memory_config") = std::nullopt,
+            py::arg("queue_id") = 0,
+        });
+}
+
+}  // namespace ttnn::operations::experimental::data_movement::detail
